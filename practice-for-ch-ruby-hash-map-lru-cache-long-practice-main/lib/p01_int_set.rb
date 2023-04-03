6c5778f9e7330fdf293530e6df3c1a5d38e317ed
rokasj <rokas@jerio.me>
2023-04-03 16:10:58 -0400

    First hashing attempt

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
@@ -1,19 +1,34 @@
 class MaxIntSet
+  attr_reader :store #completely unnecessary, besides for the spec
   def initialize(max)
+    @max = max
+    @store = []
+    booleans_to_pass = max.dup
+
+
+    until booleans_to_pass == 0
+      @store << false
+      booleans_to_pass -= 1
+    end
   end
 
   def insert(num)
+    raise ArgumentError.new "Out of bounds" unless is_valid?(num)
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num] == true
   end
 
   private
 
   def is_valid?(num)
+    num <= @max && num > 0
   end
 
   def validate!(num)
@@ -26,18 +41,28 @@ class IntSet
   end
 
   def insert(num)
+    unless include?(num)
+      idx = num % num_buckets
+      self[idx] << num
+    end
   end
 
   def remove(num)
+    if include?(num)
+      idx = num % num_buckets
+      self[idx].delete(num)
+    end
   end
 
   def include?(num)
+    idx = num % num_buckets 
+    self[idx].include?(num)
   end
 
   private
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+  def [](num) # optional but useful; return the bucket corresponding to `num`
+    @store[num]
   end
 
   def num_buckets
@@ -54,12 +79,25 @@ class ResizingIntSet
   end
 
   def insert(num)
+    unless self.include?(num)
+      resize! if num_buckets == @count
+      bucket_idx = num % num_buckets
+      self[bucket_idx] << num 
+      @count += 1
+    end
   end
 
   def remove(num)
+    if self.include?(num)
+      bucket_idx = num % num_buckets
+      self[bucket_idx].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    bucket_idx = num % num_buckets
+    self[bucket_idx].include?(num)
   end
 
   private
@@ -69,9 +107,22 @@ class ResizingIntSet
   end
 
   def resize!
+    num_temp_buckets = num_buckets * 2
+    temp_buckets = Array.new(num_temp_buckets) { Array.new }
+
+    @store.each do |bucket|
+      bucket.each do |num|
+        temp_bucket_idx = num % num_temp_buckets
+        temp_buckets[temp_bucket_idx] << num
+      end
+    end
+
+    @store = temp_buckets
   end
 
-  def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+  def [](num) # optional but useful; return the bucket corresponding to `num`
+    @store[num]
   end
+
+
 end
--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
@@ -2,8 +2,20 @@ class Integer
   # Integer#hash already implemented for you
 end
 
-class Array
+class Array #assuming just an array of integers
   def hash
+    # test = []
+    # self.each { |ele| test << ele.to_s(2) }
+
+    # test.each do |hashed_ele|
+    #   hashed_ele.to_i(2)
+    # end
+
+    [stuff]
+    hash = 100000000000000000000000000000 #1 with 29 0's
+    self.each do |ele|
+      hash = hash ^ ele
+
   end
 end
 
